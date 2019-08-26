888c95a34856fc4544388ed0b69d1362cf124ab1
fvanroie <fvanroie@users.noreply.github.com>
2019-08-26 12:06:18 +0200

    Moved to Common versions.

---
+++ NextionFontEditor/ZiLib/FileVersion/Common/FontOrientation.cs
@@ -0,0 +1,10 @@
+﻿namespace ZiLib.FileVersion.Common {
+
+    public enum FontOrientation : byte {
+        Vertical = 0x0a,
+        Horizontal = 0x0b,
+        RotatedVertical = 0x0c,
+        RotatedHorizontal = 0x0d
+    }
+
+}
